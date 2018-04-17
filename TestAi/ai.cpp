931e93ee40656017e3342ab2aa1a5939688c6c4c
Administrator <Administrator@TSINGHUA-PC>
2018-04-17 18:09:28 +0800

    [update] test ai

--- TestAi/ai.cpp
+++ TestAi/ai.cpp
@@ -33,58 +33,21 @@ PlayerControl bad_ai(const Info info) {
 
 Point<TCoor> target_coors[] = {
     Point<TCoor>(200, 75),
-    Point<TCoor>(200, 150),
-    Point<TCoor>(200, 20),
-    Point<TCoor>(200, 200),
-    Point<TCoor>(20, 20)
+    Point<TCoor>(200, 225),
+    Point<TCoor>(20, 20),
+    Point<TCoor>(20, 280)
 };
 int target_id = 0;
-const int CNT_TARGET = 5;
-PlayerControl rush_ai(const Info info) {
+const int CNT_TARGET = 4;
 
-    // 跑到 (200,75) 之后转圈
-    
-    Point<TCoor> target_coor = target_coors[target_id];
 
+PlayerControl rush_target(const Info info, Point<TCoor> target_coor) {
     // 已经到了
-    if (target_coor.getDistance(info.coor) < 30) {
-        // 下一个点
-        ++target_id; target_id %= CNT_TARGET;
-        cout << target_id << endl;
+    if (target_coor.getDistance(info.coor) < RADIUS_PROP) {
         return PlayerControl{ 0, 0, 0, NoAction };
     }
 
     TAngle target_angle = atan2_d(target_coor.y - info.coor.y, target_coor.x - info.coor.x);
-    TAngle delta_angle = fmod(target_angle - info.car_angle + 360, 360);
-    if (delta_angle > 180) delta_angle -= 360;
-
-
-    if (abs(delta_angle) < 10) {
-        // 直接跑
-        return PlayerControl{ 50, 50, 0, NoAction };
-    }
-    else {
-        
-        if (delta_angle > 0) {
-            // 左转
-            return PlayerControl{ -20, 20, 0, NoAction};
-        }
-        else {
-            // 右转
-            return PlayerControl{ 20, -20, 0, NoAction };
-        }
-    }
-
-}
-
-
-PlayerControl rush_target(const Info info, Point<TCoor> coor) {
-    // 已经到了
-    if (coor.getDistance(info.coor) < 30) {
-        return PlayerControl{ 0, 0, 0, NoAction };
-    }
-
-    TAngle target_angle = atan2_d(coor.y - info.coor.y, coor.x - info.coor.x);
     TAngle delta_angle = fmod(target_angle - info.car_angle + 360, 360);
     if (delta_angle > 180) delta_angle -= 360;
 
@@ -93,21 +56,36 @@ PlayerControl rush_target(const Info info, Point<TCoor> coor) {
 
     if (abs(delta_angle) < 10) {
         // 直接跑
-        return PlayerControl{ 50, 50, delta_attack_angle, NoAction };
+        return PlayerControl{ 50, 50, 0, NoAction };
     }
     else {
 
         if (delta_angle > 0) {
             // 左转
-            return PlayerControl{ -20, 20, delta_attack_angle, NoAction };
+            return PlayerControl{ -25, 25, 0, NoAction };
         }
         else {
             // 右转
-            return PlayerControl{ 20, -20, delta_attack_angle, NoAction };
+            return PlayerControl{ 25, -25, 0, NoAction };
         }
     }
 
 }
+
+PlayerControl rush_ai(const Info info) {
+
+
+    Point<TCoor> target_coor = target_coors[target_id];
+    if (target_coor.getDistance(info.coor) < RADIUS_PROP) {
+        // 下一个点
+        ++target_id; target_id %= CNT_TARGET;
+        return PlayerControl{ 0, 0, 0, NoAction };
+    }
+
+    return rush_target(info, target_coor);
+
+}
+
 
 PlayerControl attack_ai(const Info info) {
     static int target_index = 0;
@@ -119,34 +97,25 @@ PlayerControl attack_ai(const Info info) {
     {
         target_coor = info.cars[0].coor;
     }
-    else if (info.props.size() > 0)
-    {
-        target_coor = info.props[0].pos;
-    }
     else
     {
-        ++target_id; target_id %= CNT_TARGET;
-        cout << target_id << endl;
         target_coor = target_coors[target_id];
+        if (target_coor.getDistance(info.coor) < RADIUS_PROP) {
+            // 下一个点
+            ++target_id; target_id %= CNT_TARGET;
+        }
+        
     }
     pc = rush_target(info, target_coor);
 
     // 攻击，防御，换弹
-    if (info.cars.size() > 0)
-    {
-        TAngle target_angle = atan2_d(info.cars[0].coor, info.coor) + 360.0 - info.attack_angle - info.car_angle;
+    if (info.mp >= BUFF_MP[BUFF_DEFEND] && info.can_shd && !info.shd_status)
+        pc.action = Shield;
+    else if (info.mag > 0)
+        pc.action = Attack1;
+    else if (info.mag == 0)
+        pc.action = ChangeMag;
 
-        if (info.mag > 0 && abs(target_angle) < 3.0 / 180.0*3.1416)
-            pc.action = Attack1;
-        else if (info.mp >= BUFF_MP[BUFF_DEFEND])
-            pc.action = Shield;
-        else if (info.mag == 0)
-            pc.action = ChangeMag;
-    }
-    else {
-        if (info.mag < MAG_MAX)
-            pc.action = ChangeMag;
-    }
 
     return pc;
 }
